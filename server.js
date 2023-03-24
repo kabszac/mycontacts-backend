1a8148c8bcec20a6fbdfe99ba6d18d956b6deb30
Isaac Kabucho <kabszac@gmail.com>
2023-03-24 07:32:24 +0300

    Added contact routes and crud routes setup

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-
+const contact = require('./routes/contactRoutes')
 
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use('/api/contacts', contact)
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
