35a900847a1195a66eb13b4b1dfaef059db71124
Isaac Kabucho <kabszac@gmail.com>
2023-03-27 23:13:54 +0300

    updating and deleting CRUD

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -40,14 +40,31 @@ const postContact = asyncHandler(async(req, res) => {
 //@route PUT api/contacts/:id
 //@access public
 const updateContact =asyncHandler(async(req, res) => {
-    res.status(200).json({"message":`Update contact for ${req.params.id}`})
+    const contact = await Contact.findById(req.params.id)
+    if (!contact){
+        res.status(404)
+        throw new Error('Contact not found')
+    }
+    const updatedContent = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true}
+    )
+    res.status(200).json(updatedContent)
 })
 
 //@desc Delete a contact
 //@route DELETE api/contacts/:id
 //@access public
 const deleteContact = asyncHandler(async(req, res) => {
-    res.status(200).json({"message":`Delete contact for ${req.params.id}`})
+    const contact = await Contact.findById(req.params.id)
+    if (!contact){
+        res.status(404)
+        throw new Error('Contact not found')
+    }
+
+    await Contact.deleteOne({ _id: req.params.id });
+    res.status(200).json(contact)
 })
 
 module.exports= {
